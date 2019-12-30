e989e47fff4e0f31527aeb0df1ff2d28e458bcb5
Tarik <tarikishaq@hotmail.com>
2019-12-30 11:06:58 -0800

    uses _ seporator and the + operator in gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use chrono::{DateTime, Utc, Duration};
 // Returns a Utc DateTime one billion seconds after start.
 // Panics if it overflows
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.checked_add_signed(Duration::seconds(1000000000)).unwrap()
+    start + Duration::seconds(1_000_000_000)
 }
