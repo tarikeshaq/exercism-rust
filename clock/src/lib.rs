c9a947677b83730ca21b57977da1a3f620290fe6
Tarik <tarikishaq@hotmail.com>
2019-12-31 13:06:24 -0800

    Modified clock based on mentor feedback store only minutes

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,64 +1,30 @@
 use std::fmt::{Display, Formatter, Result};
 
-fn fix_time(time: i32, max_count: i32) -> u8 {
-    let time = time % max_count;
-    if time >= 0 {
-        time as u8
-    } else {
-        ((max_count + time) % max_count) as u8
-    }
-}
-
-fn normalize_time(hours: i32, minutes: i32) -> (u8, u8) {
-    let hours_diff = minutes / 60;
-    let mut hours = hours + hours_diff;
-    let minutes = minutes % 60;
-    if minutes < 0 {
-        hours -= 1;
-    }
-    (fix_time(hours, 24), fix_time(minutes, 60))
-}
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: u8,
-    minutes: u8,
+    minutes: u32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let (hours, minutes) = normalize_time(hours, minutes);
-        Clock { hours, minutes }
+        let mut minutes = (hours * 60 + minutes) % 1_440;
+        if minutes < 0 {
+            minutes += 1_440;
+        }
+        Clock {
+            minutes: minutes as u32,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours as i32, minutes + self.minutes as i32)
-    }
-}
-
-fn format_time(time: u8) -> String {
-    if time > 9 {
-        time.to_string()
-    } else {
-        let mut res = String::from("0");
-        res.push_str(&time.to_string());
-        res
+        Clock::new(0, minutes + (self.minutes as i32))
     }
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(
-            f,
-            "{}:{}",
-            format_time(self.hours),
-            format_time(self.minutes)
-        )
-    }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.hours == other.hours && self.minutes == other.minutes
+        let hours = (self.minutes / 60) % 24;
+        let minutes = (self.minutes - (hours * 60)) % 60;
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
