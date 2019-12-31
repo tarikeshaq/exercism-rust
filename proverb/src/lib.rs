ade23881b4a4679090ccb1e4daf26b370318be5b
Tarik <tarikishaq@hotmail.com>
2019-12-30 16:04:43 -0800

    Fix some clippy and fmt on proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -2,10 +2,14 @@ pub fn build_proverb(list: &[&str]) -> String {
     let mut res = String::new();
     for (i, word) in list.iter().enumerate() {
         if i != list.len() - 1 {
-            res.push_str(&format!("For want of a {} the {} was lost.\n", word, list[i+1]));
+            res.push_str(&format!(
+                "For want of a {} the {} was lost.\n",
+                word,
+                list[i + 1]
+            ));
         }
     }
-    if list.len() != 0 {
+    if !list.is_empty() {
         res.push_str(&format!("And all for the want of a {}.", list[0]));
     }
     res
