157be7ac9fbd4a42e18709415856acfcbe0d2b2d
Tarik <tarikishaq@hotmail.com>
2019-12-30 22:02:24 -0800

    Convert saddle-points to use iter functions

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,22 +1,11 @@
 fn is_saddle(i: usize, j: usize, input: &[Vec<u64>]) -> bool {
-    let val = input[i][j];
-    for row in input {
-        if row[j] < val {
-            return false;
-        }
-    }
-    let row = &input[i];
-    for &value in row {
-        if value > val {
-            return false;
-        }
-    }
-    true
+    let value = input[i][j];
+    input.iter().all(|row| row[j] >= value) && input[i].iter().all(|&val| val <= value)
 }
 
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut res = Vec::new();
-    for (i,row) in input.iter().enumerate() {
+    for (i, row) in input.iter().enumerate() {
         for (j, _) in row.iter().enumerate() {
             if is_saddle(i, j, input) {
                 res.push((i, j));
